1d17266226f655bf17e489bf0e6125294908f94c
Abiram Sundarraj <abiramsundar@gmail.com>
2019-04-12 22:46:58 +0530

    Basic things - first program

---
+++ Hello-World-and-very-basic.py
@@ -0,0 +1,12 @@
+print ("Hello world!") #print a string
+print ("3+5") #integer-strings
+print (3+5) #Addition
+print (3-3) #subraction
+print (3*3) #multiplication
+print (3**3) #a(3) to the power of b(3)
+print (3/3) #divide the intergers
+print (3//3) #Floor operator
+print (4%3) #Modular operator
+print ("Hello "*3) #multiply string
+print ('Abiram\'s code') #print with single quotes, escape sequence
+print (r'C:\User\new\Desktop') #process the raw string
