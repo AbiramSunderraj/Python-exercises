5c8c9da7768f1793dfac63476a633e75b2314d70
Abiram Sundarraj <abiramsundar@gmail.com>
2019-04-13 23:57:45 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,23 @@
+"""
+Created on Sat Apr 13 23:47:00 2019
+
+@author: abiram
+"""
+
+x = 3 #assign 3 for variable x
+y = 7 #assign 7 for variable y
+print(x)
+print(y)
+print("x+y") #string function
+print(x+y) #integer function
+X=10    #variable name is case sensitive
+print(X+y)
+
+
+
+#OUTPUT
+#3
+#7
+#x+y
+#10
+#17
