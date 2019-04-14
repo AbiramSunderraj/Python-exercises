6ddcbbbfa2ae0139d9f03b58a503b359f447620e
Abiram Sundarraj <abiramsundar@gmail.com>
2019-04-14 23:51:51 +0530

    Update 3 Lists.py

--- 3 Lists.py
+++ 3 Lists.py
@@ -17,6 +17,23 @@ misc = [2.67,"Abiram",35] #also can be in a mixed types
 print(misc)
 print(val, misc, val)
 
+#using some inbuilt functions
+val.append(87) #Appending 87 at the end of list
+print(val) 
+val.insert(3,666) #Inserting values at the 3rd index position
+print(val)
+val.extend([25,35,45]) #Extending the list
+print(val)
+val.pop() #Used to remove an element, in this case it will remove the last element
+print(val)
+val.pop(1) #removes 1st element
+print(val)
+val.sort() #Sorting from min to max
+print(val)
+mini = min(val) #Getting minimum value
+maxi = max(val) #Getting maximum value
+print(mini, maxi)
+
 
 
 
@@ -28,3 +45,10 @@ print(val, misc, val)
 #['starscream', 'optimus', 'bee']
 #[2.67, 'Abiram', 35]
 #[10, 25, 68, 54, 321] [2.67, 'Abiram', 35] [10, 25, 68, 54, 321]
+#[10, 25, 68, 54, 321, 87]
+#[10, 25, 68, 666, 54, 321, 87]
+#[10, 25, 68, 666, 54, 321, 87, 25, 35, 45]
+#[10, 25, 68, 666, 54, 321, 87, 25, 35]
+#[10, 68, 666, 54, 321, 87, 25, 35]
+#[10, 25, 35, 54, 68, 87, 321, 666]
+#10 666
