c2f1e6bb4379493d080dd32b8a23c0d9e6f1381b
Abiram Sundarraj <abiramsundar@gmail.com>
2019-04-15 21:17:56 +0530

    Add files via upload

---
+++ 4 Tuples & Set.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 15 19:53:32 2019
+
+@author: abiram
+"""
+
+#Tuple
+a = (25,30,14,87,25,65,54) #it uses round brackets and cannot change values
+print(a)
+b=a.count(25) #counts number of occurances
+print(b)
+
+#set
+
+x={1,25,655,54,87,0,1} #uses curly braces, Faster execution over list and tuple
+print(x) #prints the output in any form, does not have a specfic form and a value never occur again
