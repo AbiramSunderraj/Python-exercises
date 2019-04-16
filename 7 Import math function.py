b2be9504efc446dfa19175c2355fbbd9ed0f389e
Abiram Sundarraj <abiramsundar@gmail.com>
2019-04-16 22:38:31 +0530

    Create 7 Import math function.py

---
+++ 7 Import math function.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 16 21:45:16 2019
+
+@author: abiram
+"""
+import math
+#we can also use import math as m or from math import ceil,floor, etc..
+
+print(math.ceil(2.2))
+print(int(math.pow(5,2)))
+print(math.floor(3.2))
+print(math.floor(2.6))
+
+#OUTPUT
+#3
+#25
+#3
+#2
